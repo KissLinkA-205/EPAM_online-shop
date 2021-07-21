ac3a0d81ed5b7d4718a68a7306540cabfa699424
KissLinkA-205 <i.angelika897@gmail.com>
2021-07-21 22:46:13 +0300

    add RequestContext

--- src/main/java/by/epam/onlineShop/connection/ConnectionFactory.java
+++ src/main/java/by/epam/onlineShop/connection/ConnectionFactory.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionFactory {
-    private static final String URL = "db.url";
+    private static final String URL = "url";
 
     static ProxyConnection createConnection() throws ConnectionException {
         ProxyConnection proxyConnection = null;
