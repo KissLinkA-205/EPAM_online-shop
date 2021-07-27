c76bbcc23b9e958b1b69a5a7d3fdb7d4c3b35ffa
KissLinkA-205 <i.angelika897@gmail.com>
2021-07-27 22:52:03 +0300

    add Dao and Service exceptions

---
+++ src/main/java/by/epam/onlineShop/exeptions/DaoException.java
@@ -0,0 +1,14 @@
+package by.epam.onlineShop.exeptions;
+
+public class DaoException extends Exception {
+    public DaoException() {
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/by/epam/onlineShop/exeptions/ServiceException.java
@@ -0,0 +1,14 @@
+package by.epam.onlineShop.exeptions;
+
+public class ServiceException extends Exception {
+    public ServiceException() {
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
