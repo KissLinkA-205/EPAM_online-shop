7493cda6bbf54a3b21ae4e8e324cf4dec692513a
KissLinkA-205 <i.angelika897@gmail.com>
2021-08-07 23:25:43 +0300

    remove unnecessary commands

--- src/main/java/by/epam/onlineShop/logic/service/ContactsService.java
+++
@@ -1,9 +0,0 @@
-package by.epam.onlineShop.logic.service;
-
-import by.epam.onlineShop.dao.helper.DaoHelperFactory;
-
-public class ContactsService extends AbstractService {
-    public ContactsService(DaoHelperFactory daoHelperFactory) {
-        super(daoHelperFactory);
-    }
-}
